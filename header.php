5bc8181821db3af7345f24149d1d57549df07ccf
saki <73070512+sk-style@users.noreply.github.com>
2020-11-22 14:31:59 +0900

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="<?php echo get_template_directory_uri() ?>/img/favicon.ico">
     <?php wp_head(); ?>
 </head>
 
